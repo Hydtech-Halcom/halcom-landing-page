b333505f6406e867ca6fdff77bbe4c91efcd34a2
gybran nauval <nauvalgybran@gmail.com>
2026-01-30 16:23:34 +0700

    feat(Teams) : Create page and `ExpertTeamsSection`.

---
+++ app/teams/page.tsx
@@ -0,0 +1,11 @@
+import ExpertTeamsSection from "@/components/sections/teams/expert-teams-section";
+
+export default function TeamsPage() {
+  return (
+    <section className="min-h-screen py-2 md:pt-12">
+      <div className="container mx-auto px-4">
+        <ExpertTeamsSection />
+      </div>
+    </section>
+  );
+}
