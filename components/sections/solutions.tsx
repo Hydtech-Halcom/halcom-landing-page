337341246a2f9821e3d9255a8178e1d0b8e19baa
andiisultaan <andiisultaan@gmail.com>
2026-02-18 15:41:50 +0700

    fix: update solution descriptions for clarity and improved messaging

--- components/sections/solutions.tsx
+++ components/sections/solutions.tsx
@@ -7,7 +7,7 @@ import { TextAnimate } from "../ui/text-animate";
 const solutions: iCardItem[] = [
   {
     title: "IT Infrastructure",
-    description: "IT infrastructure, also called technology infrastructure, is what enables a company to build and run the applications that underpin its business.",
+    description: "Halcom delivers comprehensive IT infrastructure solutions from assessmentand design to full implementation and integration.",
     tag: "Design",
     src: "/assets/why-choose/bank-backed.jpg",
     link: "#contact",
@@ -16,7 +16,7 @@ const solutions: iCardItem[] = [
   },
   {
     title: "Physical & Cyber Security",
-    description: "IT perimeter security is the process of securing a network perimeter by implementing a combination of software, devices, and techniques.",
+    description: "We provide comprehensive physical and cybersecurity solutions, combining surveillance systems, access control, and advanced cybersecurity technologies to protect your critical infrastructure and digital assets.",
     tag: "Solutions",
     src: "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?q=80&w=2000&auto=format&fit=crop",
     link: "#contact",
@@ -25,7 +25,7 @@ const solutions: iCardItem[] = [
   },
   {
     title: "Managed Service & Maintenance",
-    description: "As part of IT Solution for your needs, we committed to give best services through our managed services and maintenance services solution.",
+    description: "As part of IT Solution for your needs, we provide comprehensive managed  services and maintenance solutions to ensure the performance, reliability, and continuity of your company.",
     tag: "Management",
     src: "https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2000&auto=format&fit=crop",
     link: "#contact",
@@ -34,7 +34,7 @@ const solutions: iCardItem[] = [
   },
   {
     title: "Mechanical Electrical Cabling Services",
-    description: "To achieved end to end solution to our customer, we provide cabling solution services for your IT and Production Infrastructure.",
+    description: " We are committed to delivering the best services through our end-to-end  mechanical, electrical, and structured cabling solutions to support reliable IT infrastructure and production environments.",
     tag: "Compliance",
     src: "https://images.unsplash.com/photo-1544197150-b99a580bb7a8?q=80&w=2000&auto=format&fit=crop",
     link: "#contact",
@@ -43,7 +43,7 @@ const solutions: iCardItem[] = [
   },
   {
     title: "Display and Audio",
-    description: "includes compute, network, workplace, data platform and edge capabilities.",
+    description: "We design and implement end-to-end display and audiovisual systems,  including video walls, digital signage, conference room solutions, and event space installations.",
     tag: "Compliance",
     src: "https://images.unsplash.com/photo-1634532843953-a4fe7334b50d?q=80&w=2000&auto=format&fit=crop",
     link: "#contact",
@@ -52,7 +52,7 @@ const solutions: iCardItem[] = [
   },
   {
     title: "Smart Meeting",
-    description: "Our smart meeting room solution integrates advanced audiovisual systems and seamless collaboration tools, enabling efficient discussions, remote connectivity, and decision-making for university teams and stakeholders.",
+    description: "Our smart meeting solutions integrate audiovisual technology and hybrid  collaboration tools to create productive, connected, and technology-driven meeting environments.",
     tag: "Compliance",
     src: "https://images.unsplash.com/photo-1631246957572-0c49e4ee6ff4?q=80&w=2000&auto=format&fit=crop",
     link: "#contact",
